d3ee29cedd453a541c02fd73b4cd24080e0d021b
sinskiy <sinskiy@proton.me>
2024-08-19 10:29:34 +0300

    feat: add index and new routers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const path = require("node:path");
 
 const express = require("express");
+const indexRouter = require("./routes/indexRoutes");
+const newRouter = require("./routes/newRoutes");
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -9,7 +11,8 @@ app.set("view engine", "ejs");
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
-app.get("/", (req, res) => res.render("index"));
+app.use("/", indexRouter);
+app.use("/new", newRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`server is running at port ${PORT}`));
