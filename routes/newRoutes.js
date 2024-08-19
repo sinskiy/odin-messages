bb150cc059ddfe537478ca884d02a5df87370017
sinskiy <sinskiy@proton.me>
2024-08-19 11:03:10 +0300

    feat: add form on /new route

--- routes/newRoutes.js
+++ routes/newRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/", (req, res) => res.send("new message"));
+router.get("/", (req, res) => res.render("form", { title: "Add new message" }));
 
 module.exports = router;
