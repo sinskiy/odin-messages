b8a038e73bf88c27decb8291d4648488276d1ffa
sinskiy <sinskiy@proton.me>
2024-08-19 10:43:01 +0300

    feat: display messages

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,6 +1,21 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/", (req, res) => res.send("index"));
+router.get("/", (req, res) =>
+  res.render("index", { title: "Mini Messageboard", messages })
+);
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 module.exports = router;
